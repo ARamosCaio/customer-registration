515495efd9d014f8ddc19bcbd1068c1865da6df9
Caio Ramos <aramoscaio@gmail.com>
2023-05-10 21:55:31 -0300

    feat: Added name entry

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class Application():
         self.root.geometry("700x500")
         self.root.resizable(True, True)
         self.root.maxsize(width=900, height=700)
-        self.root.minsize(width=400, height=300)
+        self.root.minsize(width=500, height=400)
     
      
     def screen_frames(self):
@@ -43,10 +43,18 @@ class Application():
         self.delete_btn = Button(self.top_frame, text="Apagar")
         self.delete_btn.place(relx=0.8, rely=0.1, relwidth=0.1, relheight=0.15)
 
+
         self.code_label = Label(self.top_frame, text="Código")
         self.code_label.place(relx=0.05, rely=0.05)
 
         self.code_insert = Entry(self.top_frame)
         self.code_insert.place(relx=0.05, rely=0.15, relwidth=0.08, relheight=0.1)
+
+        
+        self.name_label = Label(self.top_frame, text="Nome")
+        self.name_label.place(relx=0.05, rely=0.35)
+
+        self.name_insert = Entry(self.top_frame)
+        self.name_insert.place(relx=0.05, rely=0.45, relwidth=0.8, relheight=0.1)
 
 Application()
